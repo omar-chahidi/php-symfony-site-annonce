7b2fb8a0bbf188bccef7e7c93954761fb4d0d0ed
omar <omarchahidi@outlook.fr>
2019-12-27 19:42:30 +0100

    4 eme commit:  - installation la bibliothèque alice  - création des fixtures  - remplissage des tables

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,18 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $loader = new NativeLoader();
+        $objects = $loader->loadFile(__DIR__.'/alice/fixtures.yml')->getObjects();
+
+        foreach ($objects as $entity){
+            $manager->persist($entity);
+        }
 
         $manager->flush();
     }
